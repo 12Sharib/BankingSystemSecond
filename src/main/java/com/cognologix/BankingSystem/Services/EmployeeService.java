61ccf74a99db3d74cb9fb43fecde7097bf11bccf
Sharib Saifi <www.SharibSaifi.SS@gmail.com>
2022-11-22 15:35:33 +0530

    delted code

--- src/main/java/com/cognologix/BankingSystem/Services/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.cognologix.BankingSystem.Services;
-
-import com.cognologix.BankingSystem.Model.Employee;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface EmployeeService {
-    Employee createEmployee(Employee employee);
-}
